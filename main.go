f771b48b3f82ef097ad08f64e1301fb5f5d3e5a1
mukul_verma <aryanv018@gmail.com>
2025-02-20 09:00:36 +0530

    Implemented the cron job for marking the missed weekly payments

--- main.go
+++ main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/sharkx018/billing-engine/internal/constant"
 	"github.com/sharkx018/billing-engine/internal/handler"
+	"github.com/sharkx018/billing-engine/internal/logger"
 	"github.com/sharkx018/billing-engine/internal/middleware"
 	"github.com/sharkx018/billing-engine/internal/repo"
+	"github.com/sharkx018/billing-engine/internal/store"
 	"github.com/sharkx018/billing-engine/internal/usecase/billing"
 	"github.com/sharkx018/billing-engine/internal/usecase/user"
 	"net/http"
+	"time"
 )
+
+func RunCronJob() {
+	ticker := time.NewTicker(24 * time.Hour)
+	defer ticker.Stop()
+
+	for {
+		<-ticker.C
+		store.GlobalStore.Mu.Lock()
+		fmt.Println("Running daily EMI status update job...")
+		currentDate := time.Now().Format("2006-01-02")
+		for loanID, loan := range store.GlobalStore.Loans {
+			for i, emi := range loan.EMISchedule {
+				if emi.Status == store.Pending && emi.DueDate < currentDate {
+					currLoan := store.GlobalStore.Loans[loanID]
+					currLoan.EMISchedule[i].Status = store.Missed
+					currLoan.MissedPayments++
+					store.GlobalStore.Loans[loanID] = currLoan
+				}
+			}
+		}
+		store.GlobalStore.Mu.Unlock()
+	}
+}
 
 func main() {
 
-	fmt.Println("Billing Engine!")
+	logger.LogInfo(context.Background(), "Billing Engine! started")
+
+	//Cron job started
+	logger.LogInfo(context.Background(), "Cron job started for marking the missed payments", "")
+	go RunCronJob()
 
 	dataSourceRepo := repo.NewResource()
 
